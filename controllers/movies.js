cc58a66c5d7ab319d243685d9de687323b5b41d4
Jarom Edwards <jaromjedwards@gmail.com>
2024-01-26 15:43:23 -0700

    f

--- controllers/movies.js
+++ controllers/movies.js
@@ -4,27 +4,17 @@ const mongodb = require("../data/database");
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-    try {
-        const result = await mongodb.getDatabase().db().collection('Movies').find().toArray();
-        res.setHeader('Content-Type', 'application/json');
-        res.status(204).json(result);
-    } catch (error) {
-        console.error('Error fetching all movies:', error);
-        res.status(500).json('Internal server error');
-    }
+    const result = await mongodb.getDatabase().db().collection('Movies').find().toArray();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(204).json(result);
 }
 
 const getSingle = async (req, res) => {
-    try {
-        const movieId = new ObjectId(req.params.id);
-        const db = mongodb.getDatabase();
-        const movie = await db.db().collection('Movies').findOne({_id: movieId});
-        res.setHeader('Content-Type', 'application/json');
-        res.status(204).json(movie);
-    } catch (error) {
-        console.error('Error fetching a single movie:', error);
-        res.status(500).json('Internal server error');
-    }
+    const movieId = new ObjectId(req.params.id);
+    const db = mongodb.getDatabase();
+    const movie = await db.db().collection('Movies').findOne({_id: movieId});
+    res.setHeader('Content-Type', 'application/json');
+    res.status(204).json(movie);
 }
 
 const createMovie = async (req, res) => {
@@ -52,42 +42,33 @@ const createMovie = async (req, res) => {
     }
 };
 
+
 const updateMovie = async (req, res) => {
-    try {
-        const movieId = new ObjectId(req.params.id);
-        const movie = {
-            movieTitle: req.body.movieTitle,
-            movieDirector: req.body.movieDirector,
-            leadActor: req.body.leadActor,
-            supportingActor: req.body.supportingActor,
-            synopsis: req.body.synopsis,
-            beginning: req.body.beginning,
-            ending: req.body.ending
-        };
-        const response = await mongodb.getDatabase().db().collection('Movies').replaceOne({_id: movieId}, movie);
-        if (response.modifiedCount > 0) {
-            res.status(204).send();
-        } else {
-            res.status(500).json(response.error || 'No movie updated');
-        }
-    } catch (error) {
-        console.error('Error updating movie:', error);
-        res.status(500).json('Internal server error');
+    const movieId = new ObjectId(req.params.id);
+    const movie = {
+        movieTitle: req.body.movieTitle,
+        movieDirector: req.body.movieDirector,
+        leadActor: req.body.leadActor,
+        supportingActor: req.body.supportingActor,
+        synopsis: req.body.synopsis,
+        beginning: req.body.beginning,
+        ending: req.body.ending
+    };
+    const response = await mongodb.getDatabase().db().collection('Movies').replaceOne({_id: movieId}, movie);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'No movie updated');
     }
 };
 
 const deleteMovie = async (req, res) => {
-    try {
-        const movieId = new ObjectId(req.params.id);
-        const response = await mongodb.getDatabase().db().collection('Movies').deleteOne({_id: movieId});
-        if (response.deletedCount > 0) {
-            res.status(204).send();
-        } else {
-            res.status(500).json(response.error || 'Some error occurred while deleting the movie');
-        }
-    } catch (error) {
-        console.error('Error deleting movie:', error);
-        res.status(500).json('Internal server error');
+    const movieId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('Movies').deleteOne({_id: movieId});
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occured while creating the movie');
     }
 };
 
